9685de70ec8577d8588c2931a07fdc4ed7356251
muhammetbesdokuz <muhammetbesdokuz@gmail.com>
2022-07-19 23:16:11 +0300

    sqrt finder

---
+++ sqrt-finder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace marh2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double val, result;
+            System.Console.Write("enter a number:");
+            val = double.Parse(System.Console.ReadLine());
+
+            result = System.Math.Sqrt(val);
+
+            System.Console.WriteLine("square root of the number {0} is {1}", val, result);
+        }
+    }
+}
